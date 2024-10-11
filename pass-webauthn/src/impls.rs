0e85e370ff85959cbd484ccc7884de56ec3b0ea5
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-10-10 21:55:16 -0500

    change(pass-webauthn): base implementations

--- pass-webauthn/src/impls.rs
+++ pass-webauthn/src/impls.rs
@@ -1,21 +1,20 @@
 use frame_support::Parameter;
-use traits_authn::{Challenger, DeviceChallengeResponse, UserChallengeResponse};
+use traits_authn::{DeviceChallengeResponse, UserChallengeResponse};
 use verifier::webauthn_verify;
 
-use crate::{Assertions, Attestation};
-
-impl AsRef<[u8]> for Assertions {
-    fn as_ref(&self) -> &[u8] {
-        // See https://www.w3.org/TR/webauthn/#clientdatajson-serialization for more details
-        todo!("Concatenate assertions into the desired `authenticator_data`")
-    }
-}
+use crate::{Attestation, Credential};
 
 impl<Cx> DeviceChallengeResponse<Cx> for Attestation<Cx>
 where
     Cx: Parameter + Copy + 'static,
 {
     fn is_valid(&self) -> bool {
+        // TODO: Add sanity tests to verify that the information that's in the structure
+        // (`challenge_info`, `rp_id`, `device_id`) corresponds to the information in
+        // `authenticator_data` and `client_data`, and all that is congruent.
+        //
+        // Note: This might require using a json parsing library?
+
         webauthn_verify(
             self.authenticator_data.as_ref(),
             &self.client_data,
@@ -26,7 +25,7 @@ where
     }
 
     fn used_challenge(&self) -> (Cx, traits_authn::Challenge) {
-        (self.context, self.authenticator_data.challenge)
+        self.challenge_info
     }
 
     fn authority(&self) -> traits_authn::AuthorityId {
@@ -34,6 +33,39 @@ where
     }
 
     fn device_id(&self) -> &traits_authn::DeviceId {
-        todo!()
+        &self.device_id
+    }
+}
+
+impl<Cx> UserChallengeResponse<Cx> for Credential<Cx>
+where
+    Cx: Parameter + Copy + 'static,
+{
+    fn is_valid(&self) -> bool {
+        // TODO: Add sanity tests to verify that the information that's in the structure
+        // (`challenge_info`, `rp_id`, `device_id`) corresponds to the information in
+        // `authenticator_data` and `client_data`, and all that is congruent.
+        //
+        // Note: This might require using a json parsing library?
+
+        webauthn_verify(
+            self.authenticator_data.as_ref(),
+            &self.client_data,
+            &self.signature,
+            &self.public_key,
+        )
+        .is_ok()
+    }
+
+    fn used_challenge(&self) -> (Cx, traits_authn::Challenge) {
+        self.challenge_info
+    }
+
+    fn authority(&self) -> traits_authn::AuthorityId {
+        self.rp_id
+    }
+
+    fn user_id(&self) -> traits_authn::HashedUserId {
+        self.user_id
     }
 }
--- pass-webauthn/src/lib.rs
+++ pass-webauthn/src/lib.rs
@@ -3,35 +3,31 @@
 use codec::{Decode, Encode};
 use frame_support::{DebugNoBound, Parameter};
 use scale_info::TypeInfo;
-use traits_authn::{AuthorityId, Challenge, Challenger};
+use traits_authn::{AuthorityId, Challenge, Challenger, DeviceId, HashedUserId};
 
 #[cfg(test)]
 mod tests;
 
 mod impls;
 
-type CxOf<Ch> = <Ch as Challenger>::Context;
-
-#[derive(Encode, Decode, TypeInfo, Debug, PartialEq, Eq, Clone)]
-pub struct Assertions {
-    challenge: Challenge,
-}
-
 #[derive(Encode, Decode, TypeInfo, DebugNoBound, PartialEq, Eq, Clone)]
 pub struct Attestation<Cx: Parameter> {
     pub(crate) rp_id: AuthorityId,
-    pub(crate) context: Cx,
-    pub(crate) authenticator_data: Assertions,
+    pub(crate) device_id: DeviceId,
+    pub(crate) challenge_info: (Cx, Challenge),
+    pub(crate) authenticator_data: Vec<u8>,
     pub(crate) client_data: Vec<u8>,
-    pub(crate) signature: Vec<u8>,
     pub(crate) public_key: Vec<u8>,
+    pub(crate) signature: Vec<u8>,
 }
 
-#[derive(Encode, Decode)]
+#[derive(Encode, Decode, TypeInfo, DebugNoBound, PartialEq, Eq, Clone)]
 pub struct Credential<Cx: Parameter> {
     pub(crate) rp_id: AuthorityId,
-    pub(crate) context: Cx,
-    pub(crate) authenticator_data: Assertions,
+    pub(crate) user_id: HashedUserId,
+    pub(crate) challenge_info: (Cx, Challenge),
+    pub(crate) authenticator_data: Vec<u8>,
     pub(crate) client_data: Vec<u8>,
+    pub(crate) public_key: Vec<u8>,
     pub(crate) signature: Vec<u8>,
 }
